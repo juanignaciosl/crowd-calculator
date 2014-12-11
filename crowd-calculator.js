d988385b59090ff3c0f8fc910e8aae80442dfa2c
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2014-12-11 22:38:28 +0100

    Search box

--- crowd-calculator.js
+++ crowd-calculator.js
@@ -1,12 +1,28 @@
 var map;
 
 function init() {
-  map = new L.Map('cartodb-map', {
+  var mapId = 'cartodb-map';
+
+  var mapDiv = document.getElementById(mapId);
+
+  map = new L.Map(mapId, {
     center: [41.652044, -4.728007],
     zoom: 18
   });
 
-  var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+  var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   }).addTo(map);
+
+  cartodb.createLayer(map, {
+      user_name: 'juanignaciosl',
+      type: 'cartodb',
+      sublayers: []
+  })
+  .addTo(map) 
+  .done(function(layer) {
+    var v = cdb.vis.Overlay.create('search', map.viz, {})
+    v.show();
+    mapDiv.appendChild(v.render().el);
+  });
 }
